f32b3a9bb34a25f64f14af8d9b5159cf33b2f420
Rollie321 <rrgarcia@slsu.edu.ph>
2025-09-26 20:36:25 +0800

    add signup route

--- src/controller/AuthContoller.js
+++ src/controller/AuthContoller.js
@@ -19,3 +19,20 @@ export async function login(req, res){
         response.create(true, "Login Success", data.user)
     );
 }
+
+export async function signup(req, res){
+    const { email, password } = req.body;
+    const { data, error } = await supabase.auth.signUp({email, password});
+
+    if (error){
+        return res.status(401).json(
+            response.create(false, error.message, null)
+        );
+    }
+
+    const { session, user } = data;
+
+    if (session) AuthService.setAuthCookies(res, session);
+
+    res.json(user);
+}
