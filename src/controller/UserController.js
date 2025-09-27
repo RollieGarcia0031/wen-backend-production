8f59e1b5451840bf8e4d1fe51dbf282cd4183b96
RollieGarcia0031 <garciarollie827@gmail.com>
2025-09-27 16:50:17 +0800

    add docs for User controller:

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,5 +1,15 @@
 import User from '../services/UserService'
 
+/**
+ * Returns the email, name, and id of the currently logged user
+ * 
+ * Used to display user information in profile menu
+ * @type {RouterHandler}
+ */
 export async function getOwnInfo(req, res){
     const info = await User.getInfo();
 }
+
+/**
+ * @typedef {import('../../types/RouterHandler').CustomRouterHandler} RouterHandler
+ */
