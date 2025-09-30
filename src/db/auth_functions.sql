9a350a9dd006682f4247eaf2ade172d489dd2218
RollieGarcia0031 <garciarollie827@gmail.com>
2025-09-30 22:10:29 +0800

    add auth functions for email autosave, and autoupdate of  date field

--- src/db/auth_functions.sql
+++ src/db/auth_functions.sql
@@ -15,14 +15,15 @@ CREATE POLICY "Users can update own user profile."
 
 -- trigger function upon user signup
 
-CREATE FUNCTION public.signup_handler()
+CREATE OR REPLACE FUNCTION public.signup_handler()
 RETURNS TRIGGER AS $$
 BEGIN
   INSERT INTO public.users (id, name, role)
   VALUES (
     NEW.id,
     NEW.raw_user_meta_data->>'name',
-    NEW.raw_user_meta_data->>'role'
+    NEW.raw_user_meta_data->>'role',
+    NEW.email
   );
   
   RETURN NEW;
@@ -34,3 +35,16 @@ $$ LANGUAGE plpgsql SECURITY DEFINER;
 CREATE TRIGGER on_auth_user_created
   AFTER INSERT ON auth.users
   FOR EACH ROW EXECUTE FUNCTION public.signup_handler();
+
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = now();
+  RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER update_users_updated_at
+  BEFORE UPDATE ON public.users
+  FOR EACH ROW
+  EXECUTE FUNCTION update_updated_at_column();
